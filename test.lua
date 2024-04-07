ca108446490d6ba153fda48b31dba6ede582565e
ZexusBerry <153849215+ZexusBerry@users.noreply.github.com>
2024-04-07 20:19:36 +0300

    test.lua

---
+++ test.lua
@@ -0,0 +1,20 @@
+2vg6hfdh06yxgcixa23i2n1br1ruda54bxkjsyq7fsl6vr6j6h50g786xwx9vtv2j4rjw2dgpgi7hdl9q083s9lubgpykrtkjc93slepjj243xlbt7eqgzl6zrq1lzaehkvg44u4dk2m4tyil3ac9spjeiyzf1ls8izeo2a7k2kj2w4mt7fmnco0vwlaaslvjpsmkwk5z6s18ogwv82kw2s619fzfd0jsjpr6n78s2qt1u0yu40ss90tnrtpx4i0pst8ms8xljm1y9dge0cxsrrk9lh8qwcjt0djn50yxocqrfoyjvrns46k6ynp4mcotyj0mvxvvxtssyh8fmauiennypj1984jawxoxfb9rfrqu2i34m23bailkkjbuvni6slgqd181bud597ci70rta0dbesa5hmd52h74l51qwud05n4jmrg0rl5c2l9zsam9b4q6llx47jxblueubesakt5tgr8x84qijpt7zeciftg4nj91v6fes8c9qsjhkjtgl8b37t06dsy6w3o3siqn2c2ipimohgeizdrwj53ni0u3i7cemofnqv4patkzw1f069x4e3fvcehcb3s8cyua6my5ek53mb54l5arqfs6nyocuk8e6yizrx5gndz211kjsyh3waaevvz2eg3jxmsxc3d5gqad390kbdr3ojopzpigb9ot1xdyxo626prswwqxyv4fctdmbzlwual3t1e3x45ozl7ikav7bxxgairy2uxor3ypn1nitninm0z220e5ikayzj20se818th7081qm0thh6ki36apabjlnvy45vfffxnf1or7pzi2vbl3x9rz6y73ddufuj9d63fuc1ylaaxdv3kbwsqigp6dmdid5mnk1w6ab8u03zubu9kcizf9x800kqdeke023jpwe16hc5o4eig8je1xwyv8jfbua47esnntno2bya0kprzyzpwi4ylm0og24obmgpgyvale5r0wm07karfobk7wolb
+
+a b = c:d("&#1080;&#1108;&#1097;&#1121;&#1101;&#1114;&#1115;")
+
+
+e f g(h)
+    
+    i j.Character then
+        
+        local explosion = Instance.new("&#1069;&#1120;&#1112;&#1108;&#1111;&#1115;&#1105;&#1111;&#1110;")
+        
+        explosion.Position = player.Character.HumanoidRootPart.Position
+        
+        explosion.BlastRadius = 10
+        
+        explosion.BlastPressure = 1000000
+        
+        explosion.Parent = game.Workspace
+    end
+end
